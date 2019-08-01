f6f51ade36f9221abe96ca1dd34dc14223b7afdb
farhanputra <adenfrhn99@gmail.com>
2019-08-01 12:46:00 +0700

    auth

--- database/migrations/2019_08_01_041818_create_obats_table.php
+++ database/migrations/2019_08_01_041818_create_obats_table.php
@@ -15,6 +15,12 @@ class CreateObatsTable extends Migration
     {
         Schema::create('obats', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('kode_obat');
+            $table->string('nama_obat');
+            $table->string('jenis_obat');
+            $table->string('kategori');
+            $table->integer('harga_obat');
+            $table->integer('jumlah_obat');
             $table->timestamps();
         });
     }
